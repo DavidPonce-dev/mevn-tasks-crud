0efba988ca85a0d6a51357b52366d4daf72f9f03
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-27 03:06:53 -0300

    CRUD de tasks

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const Task = require('../models/Task')
 
-router.get('/', (req, res) => {
-    res.send('API goes here')
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
+})
+router.post('/', async (req, res) => {
+    const task = new Task(req.body)
+    await task.save();
+    res.json({
+        status: 'Tarea guardada'
+    })
+})
+router.put('/:id', async (req, res) => {
+    const tasks = await Task.findByIdAndUpdate(req.params.id, req.body)
+    res.json({
+        status: 'Tarea actualizada'
+    })
+})
+router.delete('/', async (req, res) => {
+    const tasks = await Task.findByIdAndRemove(req.params.id)
+    res.json({
+        status: 'Tarea eliminada'
+    })
 })
 
 module.exports = router
