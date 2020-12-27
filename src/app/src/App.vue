03718c8de189bfab3fcf7ee68c73e1f92c39f0bd
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-27 04:33:38 -0300

    Separando en componentes

--- src/app/src/App.vue
+++ src/app/src/App.vue
@@ -8,32 +8,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-5">
-          <div class="card">
-            <div class="card-body">
-              <form @submit.prevent="detonarSubmit">
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Ingrese una tarea"
-                    v-model="task.title"
-                  />
-                </div>
-                <div class="form-group">
-                  <textarea
-                    cols="30"
-                    rows="10"
-                    class="form-control"
-                    placeholder="Ingrese una descripcion"
-                    v-model="task.description"
-                  ></textarea>
-                </div>
-                <div class="form-group">
-                  <button type="submit" class="btn btn-success">Enviar</button>
-                </div>
-              </form>
-            </div>
-          </div>
+          <AddTaskForm />
         </div>
       </div>
     </div>
@@ -41,33 +16,12 @@
 </template>
 
 <script>
+import AddTaskForm from './components/AddTaskForm.vue'
 export default {
-  data() {
-    return {
-      task: {
-        title: "",
-        description: "",
-      },
-    };
-  },
-  methods: {
-    detonarSubmit() {
-      fetch('/api/tasks', {
-        method:'POST',
-        body: JSON.stringify(this.task),
-        headers: {
-          'Accept': 'application/json',
-          'Content-type':  'application/json'
-        }
-      })
-      .then(res => res.json())
-      .then(data => console.log(data))
-
-      this.task.title = ''
-      this.task.description = ''
-    },
-  },
-};
+  components: {
+    AddTaskForm
+  }
+}
 </script>
 
 <style>
