7b02c61769a72a8997ef0aa11b3070a314d2ec82
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-27 02:06:34 -0300

    agregando rutas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 //settings
 app.set('port', process.env.PORT || 3000)
 
 //middlewares
+app.use(morgan('dev'))
+app.use(express.json())
 
 //routes
+const tasks = require('./routes/tasks')
+
+app.use('/tasks', tasks)
 
 //static files
 app.use(express.static(__dirname + '\\public'))
